55161805671b81f44dfc86af3313d22d46038dcc
NebulaBC <contact@nebulabc.net>
2022-01-21 16:03:23 -0500

    Update ports.py

--- ports.py
+++ ports.py
@@ -4,19 +4,35 @@ import sys
 if os.geteuid() != 0:
     exit('Please run this script as root!')
 else:
-    if sys.argv[1] == 'open':
-        print('Please enter the ports you wish to open as 1 2 3 (REMEMBER TO INCLUDE YOUR SSH PORT!!1!)')
-        print('This command will also drop all previous ports opened, so remember to re-open them.')
+    if sys.argv[1] == 'firstrun':
+        if exists(/etc/portctl.firstrun) == False:
+            print('Please enter the ports you wish to open as 1 2 3 (REMEMBER TO INCLUDE YOUR SSH PORT!!1!)')
+            print('This command will also drop all previous ports opened, so remember to re-open them.')
+            ports = [int(x) for x in input().split()]
+            times = len(ports)
+            num = 0
+            os.system('iptables -F')
+            os.system('iptables -P INPUT DROP')
+            for i in range(0,times):
+                os.system(f'iptables -A INPUT -p tcp --dport {ports[num]} -j ACCEPT')
+                num=num+1
+            os.system('netfilter-persistent save && netfilter-persistent reload')
+            os.system('touch /etc/portctl.firstrun')
+            print(f'Port(s) {ports} successfully opened. From now on run python3 ports.py open')
+        else:
+            print("You have already run firstrun, please run open")
+    elif:
+        print('If you have never run this script before please run python3 ports.py firstrun')
+        print('Please enter the ports you wish to open as 1 2 3')
         ports = [int(x) for x in input().split()]
         times = len(ports)
         num = 0
-        os.system('iptables -F')
-        os.system('iptables -P INPUT DROP')
         for i in range(0,times):
             os.system(f'iptables -A INPUT -p tcp --dport {ports[num]} -j ACCEPT')
             num=num+1
         os.system('netfilter-persistent save && netfilter-persistent reload')
         print(f'Port(s) {ports} successfully opened.')
+
     elif sys.argv[1] == 'close':
         print('Please enter the ports you wish to close 1 2 3')
         ports = [int(x) for x in input().split()]
@@ -29,5 +45,3 @@ else:
         print(f'Port(s) {ports} successfully closed.')
     else:
         print('Please follow up the command with open or close.')
-
-
